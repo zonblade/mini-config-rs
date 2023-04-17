83ebe64f2a0a545f26b661d2eb3b8a955120b81c
zonblade <agungzonblade@gmail.com>
2023-04-18 02:15:33 +0700

    remove comments

--- mini-config-derive/src/lib.rs
+++ mini-config-derive/src/lib.rs
@@ -2,10 +2,7 @@ use proc_macro::TokenStream;
 
 #[proc_macro_derive(Configure)]
 pub fn arc_forge_macro(input: TokenStream) -> TokenStream {
-    // Parse the input tokens as a Rust item (enum, struct, etc.)
     let ast: syn::DeriveInput = syn::parse(input).unwrap();
-
-    // Generate the implementation of the get_str and get_json functions for the given enum
     let name = &ast.ident;
     let gen = quote::quote! {
 
@@ -34,7 +31,5 @@ pub fn arc_forge_macro(input: TokenStream) -> TokenStream {
             }
         }
     };
-
-    // Return the generated code as a TokenStream
     gen.into()
 }
